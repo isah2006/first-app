feedefc8573cade6198161ff432f84220cc036f0
isah2006 <isah20064u@gmail.com>
2023-06-19 06:37:42 -0700

    added a new route

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req,res) => {
    res.send('Hello World');
 });
 
+app.get('/what', (req,res) => {
+   res.send('What is your name?');
+});
+
 app.listen(3000, () => console.log('listenning on port 3000...'));
