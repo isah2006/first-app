0f2df3e602ca3b7cd98b2adba6354944c7161527
isah2006 <isah20064u@gmail.com>
2023-06-20 06:15:15 -0700

    added get user route

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ app.get('/', (req,res) => {
    res.send('Hello World');
 });
 
+app.get('/getUser', (req,res) => {
+   res.send({
+      username: "Adamu tako",
+      mobileNumber: 90877886
+   });
+});
+
 
 app.listen(3000, () => console.log('listenning on port 3000...'));
