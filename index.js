74a810a48d24983054797781f80433e7da54d44a
isah2006 <isah20064u@gmail.com>
2023-06-20 08:12:40 -0700

    array of fruits

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+bodyParser.urlencoded({extended: false})
+app.use(bodyParser.json())
 
 app.get('/', (req,res) => {
    res.send('Hello World');
 });
+let fruits = [{id:1, fruit:"Orange"},{id:2, fruit:"Pineapple"}, {id:3, fruit:"Apple"}, {id:4, fruit:"Watermelon"}, {id:5,fruit: "Banana"}]
 
-app.get('/getUser', (req,res) => {
-   res.send({
-      username: "Adamu tako",
-      mobileNumber: 90877886
-   });
+app.get('/getFruits', (req,res) => {
+   let responseArray = fruits.filter((fruit) => fruit.id <= 3 )
+   res.send(responseArray);
+});
+
+app.post('/addUser', (req, res) => {
+   const form = req.body
+   console.log(form);
+   res.send(form);
 });
 
 
